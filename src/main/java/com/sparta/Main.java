03d1f46371abb7fb084857dfc16f817961c7fa25
yash jagani <shi7su@protonmail.ch>
2022-12-12 18:00:25 +0000

    phase 2 complete, validation + writerfactory

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -2,6 +2,8 @@ package com.sparta;
 
 import com.sparta.dao.mysql.EmployeeDAO;
 import com.sparta.entities.Employee;
+import com.sparta.model.WriterFactory;
+import com.sparta.view.UserInput;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,35 +11,36 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
         EmployeeDAO employeeDAO = EmployeeDAO.getInstance();
-        int counter2 = 0;
-        List<Employee> list2 = employeeDAO.findEmployeesByDeptAndDate("Development", LocalDate.of(1999,1, 1), LocalDate.of(2000,1,1));
-        for (Employee e: list2){
-            counter2++;
-            System.out.println(e);
-        }
-        System.out.println(counter2);
 
-//        Employee testEmployee = new Employee(1, LocalDate.of(1995,5,5), "test", "employee", "m", LocalDate.of(1995,5,6));
-//        XmlWriter xmlWriter = new XmlWriter();
-//        //  \/\/ METHOD TO TEST \/\/
-//        xmlWriter.writeTo(testEmployee, "testEmployee");
-//        //  /\/\ METHOD TO TEST /\/\
-//        ObjectMapper mapper = new ObjectMapper();
-//        XmlConverter converter = new XmlConverter();
-//        System.out.println(converter.XMLtoObject("<Employee><employeeNumber>1</employeeNumber><dob>1995-05-05</dob><firstName>test</firstName><lastName>employee</lastName><gender>m</gender><hireDate>1995-05-06</hireDate></Employee>"));
-//        System.out.println(converter.ObjectToXML(testEmployee));
-//
-//        List<Employee> all = employeeDAO.findAll();
-//        try {
-//            EmployeeDAO.getInstance().close();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        int counter = 0;
-//        for (Employee e: all) {
+        Scanner in = new Scanner(System.in);
+
+        WriterFactory wf = new WriterFactory();
+
+//        System.out.println("Please choose the department to select from: \n" +
+//                "1: Customer Service, 2: Development, 3: Finance, 4: Human Resources, 5: Marketing, 6: Production, " +
+//                "7: Quality Management, 8: Research, 9: Sales");
+
+//        String deptName = UserInput.getDepartment(in.nextInt());
+//        System.out.println("Enter from date: ");
+//        LocalDate fromDate = UserInput.dateOneValidator(in.next());
+//        System.out.println("Enter to date: ");
+//        LocalDate toDate = UserInput.dateTwoValidator(fromDate, in.next());
+
+
+        List<Employee> list2 = employeeDAO.findEmployeesByDeptAndDate("Development",
+                LocalDate.of(1999,1,1), LocalDate.of(2001,1,1));
+//        for (Employee e: list2){
 //            System.out.println(e);
-//            counter++;
 //        }
-//        System.out.println(counter);
+
+//        List<Employee> list3 = employeeDAO.findEmployeesByDeptAndDate(deptName, fromDate, toDate);
+//        for (Employee e: list3){
+//            System.out.println(e);
+//        }
+
+        // write list2 to xml/json using factory method
+        System.out.println("What file format would you like to write to (xml/JSON): ");
+        wf.createWriter(list2, validateInput(in.next())).writeTo();
+        wf.createWriter(list2, validateInput(in.next())).writeTo();
     }
 }
--- src/main/java/com/sparta/view/UserInput.java
+++ src/main/java/com/sparta/view/UserInput.java
@@ -1,27 +1,48 @@
 package com.sparta.view;
 
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.*;
+
+import static com.sparta.dao.mysql.EmployeeDAO.fromStringToLocalDate;
 
 public class UserInput {
+    private static Scanner scanner = new Scanner(System.in);
 
-    public static String getUserInput() {
-
-        Scanner scanner = new Scanner(System.in);
-        boolean valid = false;
-        String input = "";
-        while(!valid) {
-            System.out.println("Please choose Json or XML format - write json for Json or xml for Xml");
-            input = scanner.next();
-            if (input.equalsIgnoreCase("xml")) {
-                valid = true;
-            } else if(input.equalsIgnoreCase("json")) {
-                valid = true;
-            } else {
-                System.out.println("Please choose Json or XML format - write json for Json or xml for Xml");
-                input = scanner.next();
-            }
+    public static String getDepartment(int input) {
+        List<String> departments = new ArrayList<>(Arrays.asList("Customer Service", "Development", "Finance",
+                "Human Resources", "Marketing", "Production", "Quality Management", "Research", "Sales"));
+        int dept_no = input;
+        while (dept_no < 1 || dept_no > 9) {
+            System.out.println("Please enter a number between 1 and 9: ");
+            dept_no = scanner.nextInt();
         }
-        System.out.println("Thank you");
-        return input;
+        return departments.get(dept_no - 1);
+    }
+
+    public static LocalDate dateOneValidator(String date) {
+        while (!date.matches("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$")) {
+            System.out.println("Invalid date, please enter again: ");
+            date = scanner.next();
+        }
+        return fromStringToLocalDate(date);
+    }
+
+    public static LocalDate dateTwoValidator(LocalDate from, String to) {
+        while (!to.matches("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$") ||
+                fromStringToLocalDate(to).isBefore(from)) {
+            System.out.println("Input Date, please enter again: ");
+            to = scanner.next();
+        }
+        return fromStringToLocalDate(to);
+    }
+
+    public static String validateInput(String input) {
+        if ("xml".equalsIgnoreCase(input) || "json".equalsIgnoreCase(input)) {
+            return input;
+        } else {
+            System.out.print("Invalid input! Please enter 'xml' or 'json': ");
+            input = scanner.next();
+            return validateInput(input);
+        }
     }
 }
